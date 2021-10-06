0bb47e3aa00db66cef3b47a9b0c75fca7b15c5a4
poomsc <poom.genius@gmail.com>
2021-10-07 00:20:39 +0700

    solve primitive_types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,13 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(price: u64) -> u64 {
+    if price < 65 {
+        return price * 2;
+    }
+    price
+}
 
 // Don't modify this function!
 #[test]
